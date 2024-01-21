4583d87b9184594230690aa96bafb1b1f726aae8
YasuhiroIshihara <130998364+YasuhiroIshihara@users.noreply.github.com>
2024-01-21 18:39:07 +0300

    add projekt

---
+++ main.js
@@ -0,0 +1,13 @@
+function openNav() {
+  document.getElementById("aside").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("aside").style.width = "0";
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = 'flex';
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = 'none';
+}
